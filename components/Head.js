70dc77ff6d3537dd441cc5b4ca8b6d9b6060281d
hash <skydhash@gmail.com>
2017-09-08 17:08:44 -0400

    initial commit

---
+++ components/Head.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const SkyHead = () => (
+    <Head>
+        <meta charset="utf-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <title>Skydev</title>
+        <link href="./static/css/tachyons.css" rel="stylesheet"/>
+    </Head>
+);
+
+
+export default SkyHead;
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SkyHead from "../components/Head";
+
+class Index extends React.Component {
+    render() {
+        return (
+            <div>
+                <SkyHead />
+                Hello to Skydev
+            </div>
+        )
+    }
+}
+
+export default Index;
